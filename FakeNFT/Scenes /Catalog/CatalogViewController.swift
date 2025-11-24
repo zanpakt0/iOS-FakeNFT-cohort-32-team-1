8d5f25da1b03766c6d45e574153d399e132e3ac5
mono1it <host1425@gmail.com>
2025-11-24 22:44:01 +0300

    feat(tableVeiw): Сверстал таблицу для каталога

--- FakeNFT/Scenes /Catalog/CatalogViewController.swift
+++ FakeNFT/Scenes /Catalog/CatalogViewController.swift
@@ -2,6 +2,16 @@ import UIKit
 
 final class CatalogViewController: UIViewController {
     
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.layer.cornerRadius = 12
+        tableView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        tableView.layer.masksToBounds = true
+        tableView.backgroundColor = .background
+        return tableView
+    }()
+    
     private lazy var sortButton: UIButton = {
         let button = UIButton()
         button.setImage(UIImage(resource: .sortButton), for: .normal)
@@ -14,8 +24,9 @@ final class CatalogViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor.background
+        view.backgroundColor = .background
         setupSortButton()
+        setupTableView()
     }
     
     func setupSortButton() {
@@ -26,6 +37,17 @@ final class CatalogViewController: UIViewController {
             sortButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -9),
             sortButton.heightAnchor.constraint(equalToConstant: 42),
             sortButton.widthAnchor.constraint(equalToConstant: 42)
+        ])
+    }
+    
+    func setupTableView() {
+        view.addSubview(tableView)
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: sortButton.bottomAnchor, constant: 20),
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         ])
     }
     
